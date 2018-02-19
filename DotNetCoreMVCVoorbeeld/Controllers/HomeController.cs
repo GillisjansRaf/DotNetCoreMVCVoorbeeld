7a36fc5ca12653a09f6272cf7d0a9efe239dc0cf
GillisjansRaf <raf.gillisjans@student.howest.be>
2018-02-19 11:56:18 +0100

    zoek groentjes

--- DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
+++ DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
@@ -8,9 +8,23 @@ namespace DotNetCoreMVCVoorbeeld.Controllers
 {
     public class HomeController : Controller
     {
-        public string index(string name)
+        private string[] groenten = { "asperge", "rode kool", "wortel", "spruiten", "spinazie" };
+        public ViewResult index(string name)
         {
-            return $"Hallo {name}";
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeiemiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+
+            if (string.IsNullOrEmpty(zoekGroente))
+            {
+                ViewBag.Zoekresultaat = $"de gezochte groenten is de {Array.IndexOf(groenten, zoekGroente) + 1}e uit de lijst";
+            }
+
+            return View();
         }
     }
 }
